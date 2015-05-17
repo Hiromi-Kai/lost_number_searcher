c386996041ee9384bab8106e014e6aa7a10e18d8
hiromi.kai <hiromi.kai@street-academy.com>
2015-05-17 18:02:40 +0900

    メソッドをモジュールメソッド化

--- lost_number_searcher.rb
+++ lost_number_searcher.rb
@@ -2,28 +2,28 @@
 
 module LostNumberSearcher
   # ロストナンバーが１つなら要素の合計の差分で算出できる
-  module SumAlgorithm
-    def self.search(data)
+  module SumAlgorithm extend self
+    def search(data)
       (1..10000).to_a.inject(:+) - data.inject(:+)
     end
   end
 
   # Ruby標準の差集合計算
-  module RelativeComplementAlgorithm
-    def self.search(data)
+  module RelativeComplementAlgorithm extend self
+    def search(data)
       ((1..10000).to_a - data).first
     end
   end
 
   # ソートして二分探索で探す
-  module SearchAlgorithm
-    def self.search(data)
+  module SearchAlgorithm extend self
+    def search(data)
       binary_search(data.sort)
     end
 
     # 再帰的に二分探索してデータを絞り込む
     # データを二分割する過程で配列のindexと実データがずれる分をoffsetで補正する
-    def self.binary_search(data, offset = 0)
+    def binary_search(data, offset = 0)
       index = get_search_index(data.length)
       correct_data = index + offset + 1
 
@@ -36,7 +36,7 @@ module LostNumberSearcher
       end
     end
 
-    def self.get_search_index(length)
+    def get_search_index(length)
       length.even? ? (length / 2)  : (length - 1) / 2
     end
   end
