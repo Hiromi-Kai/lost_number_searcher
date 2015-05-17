dd6fe0579c552e56dd8c8b329c0973d05659e286
hiromi.kai <hiromi.kai@street-academy.com>
2015-05-17 17:06:00 +0900

    ひとまず実装完了

---
+++ lost_number_searcher.rb
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+module LostNumberSearcher
+  # ロストナンバーが１つなら要素の合計の差分で算出できる
+  module SumAlgorithm
+    def self.search(data)
+      (1..10000).to_a.inject(:+) - data.inject(:+)
+    end
+  end
+
+  # Ruby標準の差集合計算
+  module RelativeComplement
+    def self.search(data)
+      ((1..10000).to_a - data).first
+    end
+  end
+
+  # ソートして二分探索で探す
+  module SearchAlgorithm
+    def self.search(data)
+      binary_search(data.sort)
+    end
+
+    def self.binary_search(data, offset = 0)
+      index = get_search_index(data.length)
+      p index + offset
+      p data[index]
+
+      if data[index] == index + offset+ 1 && data[index + 1] != index + offset + 2
+        return index + 2 + offset
+      end
+      if data[index] == index + offset + 1
+        binary_search(data[(index + 1)..-1], index + 1 + offset)
+      else
+        binary_search(data[0...index],offset)
+      end
+    end
+
+    def self.get_search_index(length)
+      length.even? ? (length / 2)  : (length - 1) /2
+    end
+  end
+end
+
+if $0 ==  __FILE__
+  p LostNumberSearcher::SumAlgorithm.search((1..10000).to_a - [7777])
+  include LostNumberSearcher
+  p SearchAlgorithm.search((1..10000).to_a - [7777])
+end
---
+++ lost_number_searcher_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+
+require './lost_number_searcher'
+
+def get_test_data
+  lost_number = rand(10000)
+  { :lost_number => lost_number, :data => ((1..10000).to_a - [lost_number]) }
+end
+
+describe LostNumberSearcher do
+  let(:input){get_test_data}
+  context '合計の差方式' do
+    it '正常に検索ができる' do
+      include
+      result = LostNumberSearcher::SumAlgorithm.search input[:data]
+      expect(result).to eq(input[:lost_number])
+    end
+  end
+
+  context '差集合方式' do
+    it '正常に検索ができる' do
+      result = LostNumberSearcher::RelativeComplement.search input[:data]
+      expect(result).to eq(input[:lost_number])
+    end
+  end
+
+  context '二部探索方式' do
+    it '正常に検索ができる' do
+      p input[:lost_number]
+      result = LostNumberSearcher::SearchAlgorithm.search input[:data].sort
+      expect(result).to eq(input[:lost_number])
+    end
+  end
+end
